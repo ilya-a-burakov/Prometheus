42a52bd50f807ca3a95090f340642317d7aff148
Mac <mac@MacBook-Pro-Mac.local>
2022-11-22 16:44:19 +0700

    fixes

---
+++ node_modules/vue-meeting-selector/src/interfaces/ClassNames.interface.ts
@@ -0,0 +1,18 @@
+interface ClassNames {
+  tabClass?: string,
+  tabPaginationPrevious?: string,
+  tabPaginationPreviousButton?: string,
+  tabPaginationNext?: string,
+  tabPaginationNextButton?: string,
+  tabPaginationUpButton?: string,
+  tabPaginationDownButton?: string,
+  tabDays?: string,
+  tabDayDisplay?: string,
+  tabMeetings?: string,
+  tabMeeting?: string,
+  tabMeetingButton?: string,
+  tabMeetingEmpty?: string,
+  tabLoading?: string,
+}
+
+export default ClassNames;
